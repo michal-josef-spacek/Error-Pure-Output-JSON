ed30166d060e393fe55bf57ffc8d60fc81bb6110
Michal Špaček <tupinek@gmail.com>
2013-08-31 00:40:47 +0200

    Prepare for 0.03 version.

--- JSON.pm
+++ JSON.pm
@@ -16,7 +16,7 @@ Readonly::Array our @EXPORT_OK => qw(err_json);
 our $PRETTY = 0;
 
 # Version.
-our $VERSION = 0.02;
+our $VERSION = 0.03;
 
 # JSON print of backtrace.
 sub err_json {
@@ -205,6 +205,6 @@ BSD license.
 
 =head1 VERSION
 
-0.02
+0.03
 
 =cut
