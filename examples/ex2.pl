6a2e5277a48cdae32dd7e387ac14d4427067c387
Michal Špaček <tupinek@gmail.com>
2013-08-30 10:43:45 +0200

    Update examples.

--- examples/ex2.pl
+++ examples/ex2.pl
@@ -38,4 +38,28 @@ my $err_hr = {
 print err_json($err_hr);
 
 # Output:
-# TODO
+# [
+#    {
+#       "msg" : [
+#          "FOO",
+#          "KEY",
+#          "VALUE"
+#       ],
+#       "stack" : [
+#          {
+#             "sub" : "err",
+#             "prog" : "script.pl",
+#             "args" : "(2)",
+#             "class" : "main",
+#             "line" : 1
+#          },
+#          {
+#             "sub" : "eval {...}",
+#             "prog" : "script.pl",
+#             "args" : "",
+#             "class" : "main",
+#             "line" : 20
+#          }
+#       ]
+#    }
+# ]
