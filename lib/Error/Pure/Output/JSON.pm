cebddacbf9f76758a40878493bb0cd58743bd995
Michal Josef Špaček <michal.josef.spacek@gmail.com>
2023-05-11 19:19:25 +0200

    Update author name

--- lib/Error/Pure/Output/JSON.pm
+++ lib/Error/Pure/Output/JSON.pm
@@ -200,13 +200,13 @@ L<https://github.com/michal-josef-spacek/Error-Pure-Output-JSON>
 
 =head1 AUTHOR
 
-Michal Špaček L<mailto:skim@cpan.org>
+Michal Josef Špaček L<mailto:skim@cpan.org>
 
 L<http://skim.cz>
 
 =head1 LICENSE AND COPYRIGHT
 
- © Michal Špaček 2013-2015
+ © Michal Josef Špaček 2013-2015
  BSD 2-Clause License
 
 =head1 VERSION
